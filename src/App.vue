f5cf5d71f3a6d7cf6ff11505daf248dddc5cb38e
mtotodev05 <mtotodev05@gmail.com>
2018-08-10 15:12:29 +0300

    Vue-drawer-navigation

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,28 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-toolbar dark class="red darken-2">
+      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-down"></v-toolbar-side-icon>
+      <v-toolbar-title>Dev MeetUp</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-sm-only">
+        <v-btn flat v-for="item in menuItems":key="item.title">
+          <v-icon dark left>{{item.icon}}</v-icon>
+          {{item.title}}</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile @click="right = !right">
-          <v-list-tile-action>
-            <v-icon>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
+    <v-navigation-drawer v-model="sideNav">
+    <v-list>
+      <v-list-tile v-for="item in menuItems":key="item.title">
+        <v-list-tile-action>
+          <v-icon>{{item.icon}}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>{{item.title}}</v-list-tile-content>
+      </v-list-tile>
+    </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
+  <main>
+
+  </main>
   </v-app>
 </template>
 
@@ -73,19 +30,21 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Dev-meetUp Vue'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Dev-MeetUp Vue'
+        sideNav:false,
+        menuItems:[
+          {icon: 'supervisor_account' ,title:'View Meetups'},
+          {icon: 'room',title:'Organize Meetup'},
+          {icon: 'person',title:'Profile'},
+          {icon: 'face',title:'Sign up'},
+          {icon: 'lock_open',title:'Sign in'}
+
+
+        ]
     }
   },
   name: 'App'
 }
 </script>
+<style lang="stylus">
+  @import './stylus/main'
+</style>
