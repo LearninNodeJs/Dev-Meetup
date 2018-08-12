3a4fce6d5dc6fe6b2606fb62315c4063712a08aa
mtotodev05 <mtotodev05@gmail.com>
2018-08-12 21:09:14 +0300

    Added Form Validation

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,9 +1,87 @@
 <template>
   <v-container>
     <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h2 class="secondary--text">Create a new Meetup </h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
       <v-flex xs12>
-        <h2>Create a new Meetup </h2>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+              name="title"
+              label="Title"
+              id="title"
+              v-model="title"
+              required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image Url"
+                id="imageUrl"
+                v-model="imageUrl"
+                required></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+             <img :src="imageUrl" alt="" height="200"/>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="description"
+                label="Description"
+                id="description"
+                v-model="description"
+                required multi-line></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary" :disabled="!formIsValid">Create Meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
+
+<script>
+  export default {
+    data(){
+      return{
+        title:'',
+        location:'',
+        imageUrl:'',
+        description:''
+      }
+    },
+    computed:{
+      formIsValid(){
+        return this.title !=='' && this.location !=='' && this.imageUrl!=='' &&
+          this.description!=='';
+      }
+    }
+
+  }
+</script>
+
+
