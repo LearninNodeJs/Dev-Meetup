0a14bd5c4c66ee33f6dd6b318fcb5ff3e8ee6d66
mtotodev05 <mtotodev05@gmail.com>
2018-08-14 07:53:03 +0300

    fixed for environmental variables

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,10 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-
+  apiKey: '"AIzaSyAc-HKw1TBw0hrCL3j4Q-oOxXSK5VY7oMU"',
+  authDomain: '"devmeetupvue-a8f57.firebaseapp.com"',
+  databaseURL: '"https://devmeetupvue-a8f57.firebaseio.com"',
+  projectId: '"devmeetupvue-a8f57"',
+  storageBucket: '"devmeetupvue-a8f57.appspot.com"',
+  messagingSenderId: '"458070932747"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  apiKey: '"AIzaSyAc-HKw1TBw0hrCL3j4Q-oOxXSK5VY7oMU"',
+  authDomain: '"devmeetupvue-a8f57.firebaseapp.com"',
+  databaseURL: '"https://devmeetupvue-a8f57.firebaseio.com"',
+  projectId: '"devmeetupvue-a8f57"',
+  storageBucket: '"devmeetupvue-a8f57.appspot.com"',
+  messagingSenderId: '"458070932747"'
 }
